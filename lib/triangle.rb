c53a031c2b452afb08747759c8fb2490c211e7f7
tylerselldesign <tylerselldesign@gmail.com>
2018-09-26 20:46:28 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,19 @@
 class Triangle
-  # write code here
+ 
+  def initialize(a, b, c)
+    @a = a 
+    @b = b 
+    @c = c 
+    
+    if @a == @
+  end
+ 
+  def kind 
+    
+  end
+  
+  class TriangleError < StandardError
+    
+  end
+
 end
